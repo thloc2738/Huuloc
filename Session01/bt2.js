4b282f634d601482673e02ae7ad91720edaacfe3
Tran Huu Loc <69748741+thloc2738@users.noreply.github.com>
2021-11-17 15:51:56 +0700

    Add files via upload

---
+++ Session01/bt2.js
@@ -0,0 +1,30 @@
+var changeChar = ['B', 'M', 'K', ''];
+var arrNum = [1000000000, 1000000, 1000,];
+var number;
+let result="";
+let temp;
+function changeMoney(number)
+{
+    for(let i = 0; i < arrNum.length;i++)
+    {
+        if(number>=1000)
+        {
+            let a = parseInt(number/arrNum[i]) 
+            if(a != 0)
+            {
+            temp = number/arrNum[i]+'';  
+            result = temp.slice(0,4) + changeChar[i];
+            break;
+            }
+        }
+        else
+        {
+            result = number;
+        }
+        
+        
+    }
+    console.log(result);
+}
+console.log("Input: 11234000000, Output: " + changeMoney(1123400000));
+changeMoney(11234000000);
